68c04de4699132c6ee1136dfea094bb437cc218e
Afagh <afagh@Afaghs-MacBook-Pro.local>
2025-03-10 05:49:53 +0330

    init: init the structure of the project

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold mb-4">مقاله‌ها</h1>
+            <div>
+                this is where we will show articles
+            </div>
+        </div>
+    );
+}
