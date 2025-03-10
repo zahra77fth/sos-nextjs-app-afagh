8fdb2553d4c321adf996bc627fd795e204c69279
Afagh <afagh@Afaghs-MacBook-Pro.local>
2025-03-10 06:49:43 +0330

    feat: add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
-import localFont from 'next/font/local'
+import localFont from 'next/font/local';
+import Header from "@/components/Header";
 
 const IRANSansXV = localFont({ src: '../assets/fonts/IRANSansXV.woff2' })
 
@@ -12,10 +13,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="fa" dir="rtl">
-            <body className={`${IRANSansXV.className} bg-gray-50`}>
-                <header>
-                    this is header
-                </header>
+            <body className={`${IRANSansXV.className}`}>
+            <Header />
                 <main className="container mx-auto px-4 py-6">{children}</main>
             </body>
         </html>
---
+++ components/Header.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useCallback } from "react";
+import { Menu, X } from "lucide-react";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = useCallback(() => {
+        setIsOpen((prev) => !prev);
+    }, []);
+
+    return (
+        <header className="bg-white shadow-md">
+            <div className="container mx-auto flex justify-between items-center p-4">
+                <div className="flex-1 flex items-center space-x-6">
+                    <Link href="/">
+                        <Image src="/sos.svg" alt="صفحه اصلی" width={198} height={40} />
+                    </Link>
+
+                    <nav className="hidden md:flex flex-1 justify-between pl-6">
+                        <NavLink href="/">صفحه اصلی</NavLink>
+                        <NavLink href="/martyrs">شعبه‌های ما</NavLink>
+                        <NavLink href="/health-centers">مراکز خدمات درمانی</NavLink>
+                        <NavLink href="/todo">To-Do List</NavLink>
+                        <NavLink href="/faq">سوالات متداول</NavLink>
+                    </nav>
+                </div>
+
+                <Link
+                    href="/login"
+                    className="hidden md:block bg-[#1158A7] text-white px-4 py-2 rounded hover:bg-blue-600"
+                >
+                    ورود و فعالسازی
+                </Link>
+                <div className="md:hidden p-2">
+                    <IconButton onClick={toggleMenu}>
+                        <Menu size={28} className="text-[#1158A7]" />
+                    </IconButton>
+                </div>
+
+                <Drawer anchor="right" open={isOpen} onClose={toggleMenu}>
+                    <div className="p-4 w-64 bg-white h-full shadow-lg">
+                        {/* Close Button */}
+                        <IconButton onClick={toggleMenu} className="p-2">
+                            <X size={28} className="text-gray-700" />
+                        </IconButton>
+
+                        <nav className="flex flex-col mt-6 space-y-4">
+                            <NavLink href="/" onClick={toggleMenu}>
+                                صفحه اصلی
+                            </NavLink>
+                            <NavLink href="/martyrs" onClick={toggleMenu}>
+                                شعبه‌های ما
+                            </NavLink>
+                            <NavLink href="/health-centers" onClick={toggleMenu}>
+                                مراکز خدمات درمانی
+                            </NavLink>
+                            <NavLink href="/todo" onClick={toggleMenu}>
+                                To-Do List
+                            </NavLink>
+                            <NavLink href="/faq" onClick={toggleMenu} >
+                                سوالات متداول
+                            </NavLink>
+                        </nav>
+
+                        <Link
+                            href="/login"
+                            className="mt-6 block bg-[#1158A7] text-white text-center py-2 rounded hover:bg-blue-600"
+                            onClick={toggleMenu}
+                        >
+                            ورود و فعالسازی
+                        </Link>
+                    </div>
+                </Drawer>
+            </div>
+        </header>
+    );
+};
+
+const NavLink = ({ href, children, onClick }: { href: string; children: React.ReactNode; onClick?: () => void }) => (
+    <Link href={href} onClick={onClick} className="text-[#1158A7] hover:text-gray-600">
+        {children}
+    </Link>
+);
+
+export default Header;
